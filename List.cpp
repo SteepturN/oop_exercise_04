b4f41b6e305cc26f529086db97b2e65ddfb2fc48
admin <admin@gg>
2021-11-16 08:03:09 +0300

    initial commit

---
+++ List.cpp
@@ -0,0 +1,39 @@
+#include "Triangle.h"
+#include "List.h"
+#include "ListEl.h"
+#include "Iterator.h"
+
+List::List()
+    : head(new ListEl(NULL, Triangle())), size(0) {}
+List::~List() {
+    for(auto cur = this->begin(); cur != this->end();) {
+        auto del = cur;
+        ++cur;
+        delete(*(del.el));
+    }
+}
+int List::put(Triangle& t, Iterator& i) {
+    ListEl* new_list_element = new ListEl((*(i.el))->next, t);
+    *(i.el) = new_list_element;
+    return 0;
+}
+void List::remove(Iterator& i) {
+    auto del_this = i;
+    *(i.el) = ((*(i.el))->next);
+    delete(*(del_this.el));
+}
+Triangle& List::get(Iterator& i) {
+    return *i;
+}
+Iterator List::begin() {
+    return Iterator(this, &head);
+}
+Iterator List::end() {
+    return Iterator(this, NULL);
+}
+std::ostream& operator<<(std::ostream& cout, List& list) {
+    for(auto i = list.begin(); i != list.end(); ++i) {
+        cout << *i;
+    }
+    return cout;
+}
---
+++ main.cpp
@@ -0,0 +1,109 @@
+/*
+Бутырев М8О-206Б-20
+var 12
+связный список + треугольник
+Необходимо спроектировать и запрограммировать на языке C++ класс-контейнер первого уровня, содержащий одну фигуру (одна из фигур ЛР3 на выбор). Вариант структуры данных для контейнера выбрать из документа “Варианты структур данных”  (контейнер 1-го уровня) согласно своему номеру из  Варианты ЛР4..
+Классы должны удовлетворять следующим правилам:
+·   Требования к классу фигуры аналогичны требованиям из лабораторной работы 3
+·   Классы фигур должны иметь переопределенный оператор вывода в поток std::ostream (<<). Оператор должен распечатывать параметры фигуры (тип фигуры, длины сторон, радиус и т.д).
+·         Классы фигур должны иметь переопределенный оператор ввода фигуры из потока std::istream (>>). Оператор должен вводить основные параметры фигуры (длины сторон, радиус и т.д).
+·         Классы фигур должны иметь операторы копирования (=).
+·         Классы фигур должны иметь операторы сравнения с такими же фигурами (==).
+
+·         Класс-контейнер должен содержать объекты фигур “по значению” (не по ссылке).
+·         Класс-контейнер должен иметь метод по добавлению фигуры в контейнер.
+·         Класс-контейнер должен иметь методы по получению фигуры из контейнера (определяется структурой контейнера).
+·         Класс-контейнер должен иметь метод по удалению фигуры из контейнера (определяется структурой контейнера).
+·         Класс-контейнер должен иметь перегруженный оператор по выводу контейнера в поток std::ostream (<<).
+·         Класс-контейнер должен иметь деструктор, удаляющий все элементы контейнера.
+·         Классы должны быть расположены в раздельных файлах: отдельно заголовки (.h), отдельно описание методов (.cpp).
+Программа должна позволять:
+·         Вводить произвольное количество фигур и добавлять их в контейнер.
+·         Распечатывать содержимое контейнера.
+·         Удалять фигуры из контейнера.
+*/
+#include <iostream>
+#include "Triangle.h"
+#include "Read_input.cpp"
+#include "List.h"
+#include "Iterator.h"
+#include "ListEl.h"
+#include <cstdio>
+#include <set>
+#include <string>
+int main(){ // можно вынести названия команд как константы
+	std::string help_message = "You can use\n\
+--put triangle: p [(point) 3 times]\n\
+--print container: print\n\
+--delete (figure number): d (number of figure)\n\
+--exit\n";
+	List list;
+	char ch(' ');
+	char command[20];
+	std::set<std::string> valid_commands = {"p", "print", "d", "exit"};
+//"
+	std::cout << help_message;
+	do {
+	beginning_of_the_entire_input:
+		bool valid_input = false;
+		do{
+			read_return_t answer = get_command(valid_commands, command);
+			switch(answer) {
+				case END_OF_FILE: return 0;
+				case END_OF_LINE: continue;
+				case VALID_INPUT: valid_input = true; break;
+				case INVALID_INPUT:
+					do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+					std::cout << "wrong input" << std::endl;
+					if(ch == EOF) return 0;
+					else break;
+			}
+		} while(!valid_input);
+		std::string&& command_string = static_cast<std::string>(command);
+		if(command_string == "exit") return 0;
+		if(command_string == "p") {
+			int num_of_verteces = 3;
+			Triangle triangle;
+			std::pair<double, double> vertex;
+			for(int i = 0; i < num_of_verteces; i++) { //add usage of \n?
+				if(get_value<Triangle>(triangle) != VALID_INPUT) {
+					do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+					std::cout << "wrong input" << std::endl;
+					if(ch == EOF) return 0;
+					else goto beginning_of_the_entire_input;
+				}
+			}
+		} else if(command_string == "print") {
+			std::cout << list;
+		} else {
+			unsigned int input_figure_number(0);
+			if(get_value<unsigned int>(input_figure_number) != VALID_INPUT) { //if there would be EOF
+				do ch=getchar(); while((ch != EOF) && (ch != '\n'));
+				std::cout << "wrong input" << std::endl;
+				if(ch == EOF) return 0;
+				else continue;
+			} else {
+				bool all_done = false;
+				Iterator i = list.begin();
+				while(!all_done) {
+					if(i == list.end()) {
+						std::cout << "too much\n";
+						all_done = true;
+					} else if(input_figure_number == 0) {
+						list.remove(i);
+						all_done = true;
+					} else {
+						++i;
+						--input_figure_number;
+					}
+				}
+			}
+
+		}
+		do ch = getchar(); while((ch != '\n') && (ch != EOF));
+		std::cout << std::endl;
+		if(ch == EOF) return 0;
+	} while(true);
+	return 0;
+}
+
